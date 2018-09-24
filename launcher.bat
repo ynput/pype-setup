41fbb45656ca3f0f4166924b843156d8c8eecd04
Jakub Jezek <jezschaj@gmail.com>
2018-09-24 18:15:27 +0200

    before remote merge

--- launcher.bat
+++ launcher.bat
@@ -1,20 +1,21 @@
 @echo off
-set SYNC_ENV=1
+set SYNC_ENV=0
 call %~dp0bin\launch_conda.bat
 
 :: Setup Avalon
 set PATH=%~dp0app;%PATH%
 
+set AVALON_CONFIG=config
+set AVALON_LAUNCHER=%~dp0app\repos\avalon-launcher
+set AVALON_PYTHONPATH=D:\CODE\github\avalon-core-toke-nuke
+::%~dp0app\repos\avalon-core
+set AVALON_CORE=%AVALON_PYTHONPATH%
+
+set AVALON_MONGO=mongodb://localhost:27017
+
 :: Setup PYTHONPATH
 set PYTHONPATH=%~dp0app\repos\pyblish-base;%PYTHONPATH%
 set PYTHONPATH=%~dp0app\repos\pyblish-qml;%PYTHONPATH%
 set PYTHONPATH=%~dp0studio\studio-config;%PYTHONPATH%
-
-set AVALON_CONFIG=config
-set AVALON_LAUNCHER=%~dp0app\repos\avalon-launcher
-set AVALON_PYTHONPATH=%~dp0app\repos\avalon-core
-set AVALON_CORE=%AVALON_PYTHONPATH%
-
-set AVALON_MONGO=mongodb://localhost:27017
 
 start "Avalon Launcher" avalon
--- terminal.bat
+++ terminal.bat
@@ -1,2 +1,22 @@
 set REMOTE_ENV_ON=0
-start "Avalon Terminal" %~dp0bin\launch_conda.bat
+
+call %~dp0bin\launch_conda.bat
+
+:: Setup Avalon
+set PATH=%~dp0app;%PATH%
+
+set AVALON_CONFIG=config
+set AVALON_LAUNCHER=%~dp0app\repos\avalon-launcher
+set AVALON_PYTHONPATH=D:\CODE\github\avalon-core-toke-nuke
+::%~dp0app\repos\avalon-core
+set AVALON_CORE=%AVALON_PYTHONPATH%
+
+set AVALON_MONGO=mongodb://localhost:27017
+
+:: Setup PYTHONPATH
+set PYTHONPATH=%~dp0app\repos\pyblish-base;%PYTHONPATH%
+set PYTHONPATH=%~dp0app\repos\pyblish-qml;%PYTHONPATH%
+set PYTHONPATH=%~dp0studio\studio-config;%PYTHONPATH%
+
+
+start "Avalon Terminal" cmd
