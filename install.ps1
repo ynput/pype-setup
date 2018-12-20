413a8de8b4e7e5e151ec6d54b5ebcfd0622b523d
antirotor <annatar@annatar.net>
2018-12-20 22:29:33 +0100

    launch conda and pype command working

--- install.ps1
+++ install.ps1
@@ -1,3 +1,15 @@
+# Pypeclub / Pype
+# part of pype-setup
+#     ____________  ____      ____  ____________  ____________
+#   / \           \/\   \    /\   \/\           \/\           \
+#   \  \      ---  \ \   \___\_\   \ \      ---  \ \     ------\
+#    \  \     _____/  \____     ____\ \     _____/  \    \___\
+#     \  \    \__/  \____/ \    \__/\  \    \__/  \  \    -------\
+#      \  \____\         \  \____\   \  \____\     \  \___________\
+#       \/____/           \/____/     \/____/       \/___________/
+#
+#                    ...  █░░ --=[ CLuB ]]=-- ░░█ ...
+
 # install dependencies
 if (-not (Get-Module -ListAvailable -Name "PSWriteColor")) {
   Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
@@ -15,7 +27,7 @@ $env:PYPE_STUDIO_TEMPLATES_SUBM="repos"
 $env:PYPE_STUDIO_TEMPLATES_BRANCH="master"
 
 # Directory prefix for local environment
-$env:CONDA_LOCAL =  "C:\Users\Public\"
+$env:CONDA_LOCAL =  "C:\Users\Public"
 
 # Debugging
 $env:PYPE_DEBUG=1
@@ -29,11 +41,13 @@ $env:REMOTE_ENV_ON=0
 Write-Color -Text ">>> ", "Welcome to Pype Club" -Color Green, White
 Write-Color -Text ">>> ", "Launching Conda ..." -Color Green, Gray
 
+$arguments = $ARGS
 # Launch Conda
 try {
   . ("$($env:PYPE_SETUP_ROOT)\bin\launch_conda.ps1")
 }
 catch {
-  Write-Color -Text "!!! ", "Failed to launcho conda setup" -Color Red, Gray
+  Write-Color -Text "!!! ", "Failed to launch conda setup" -Color Red, Gray
   Write-Host $_.Exception.Message
 }
+exit
