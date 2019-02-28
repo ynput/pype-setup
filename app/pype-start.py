09f2df69210c0fae516393e7446a77346bf7f875
Jakub Trllo <jakub.trllo@gmail.com>
2019-02-28 14:45:13 +0100

    already running trays are killed on startup and powershell is shuted down

--- app/pype-start.py
+++ app/pype-start.py
@@ -41,6 +41,7 @@ import shutil
 import tempfile
 import contextlib
 import subprocess
+import psutil
 
 from app import api
 
@@ -225,6 +226,24 @@ def main():
         items = [pype_setup, "app", "tray.py"]
         fname = os.path.sep.join(items)
 
+        # Terminate already running tray
+        procs = [p for p in psutil.process_iter() if (
+            'python.exe' in p.name() and
+            fname in p.cmdline()
+        )]
+        if len(procs) > 0:
+            for running_proc in procs:
+                running_proc.terminate()
+
+        # Terminate powershell that runs this script
+        powershell_procs = [p for p in psutil.process_iter() if (
+            'powershell.exe' in p.name() and
+            '--tray' in p.cmdline()
+        )]
+        if len(powershell_procs) > 0:
+            for running_proc in powershell_procs:
+                running_proc.terminate()
+
         args = ["-d", fname]
         if sys.platform.startswith('linux'):
             subprocess.Popen(
@@ -238,8 +257,7 @@ def main():
                 stderr=None,
                 preexec_fn=os.setpgrp
             )
-
-        if sys.platform == 'win32':
+        elif sys.platform == 'win32':
             subprocess.Popen(
                 args,
                 universal_newlines=True,
@@ -252,6 +270,7 @@ def main():
                 stderr=subprocess.STDOUT,
                 creationflags=DETACHED_PROCESS
             )
+
     elif kwargs.traydebug:
         pype_setup = os.getenv('PYPE_SETUP_ROOT')
         items = [pype_setup, "app", "tray.py"]
