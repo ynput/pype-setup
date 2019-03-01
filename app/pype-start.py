127bddfa5ad1a1b42435f830d88719b2f5d63a32
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-01 17:14:38 +0100

    changed path to event server

--- app/pype-start.py
+++ app/pype-start.py
@@ -265,7 +265,10 @@ def main():
 
     elif kwargs.eventserver:
         pype_config = os.getenv('PYPE_STUDIO_CONFIG')
-        items = [pype_config, "pype", "ftrack", "event_server.py"]
+        items = [
+            pype_config, "pype", "ftrack", "ftrack_server",
+            "event_server.py"
+        ]
         fname = os.path.sep.join(items)
 
         returncode = api.forward([
@@ -273,8 +276,11 @@ def main():
         ] + args)
 
     elif kwargs.eventservercli:
-        event_server_path = os.getenv('FTRACK_EVENT_SERVER')
-        items = [event_server_path, "eventServer.py"]
+        pype_config = os.getenv('PYPE_STUDIO_CONFIG')
+        items = [
+            pype_config, "pype", "ftrack", "ftrack_server",
+            "event_server_cli.py"
+        ]
         fname = os.path.sep.join(items)
 
         returncode = api.forward([
