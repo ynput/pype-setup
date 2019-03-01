d49e1e0e1cc8cf003f23bf23170fcc03222b46e0
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-01 11:13:18 +0100

    added get_presets function requires environment PYPE_STUDIO_CONFIG

--- pypeapp/lib/config.py
+++ pypeapp/lib/config.py
@@ -27,3 +27,30 @@ def collect_json_path(input_path):
                 output = json.load(f)
 
     return output
+
+
+def get_presets(*args):
+    '''
+    *args specify folder or file in presets to load
+    EXAMPLE:
+    get_presets('ftrack', 'ftrack_config')
+    1.) checks if path "{config_path}/presets/ftrack/ftrack_config" is dir
+        and loops through all folders/files and load all jsons into dict
+    2.) if path is not dir then checks if exists file:
+        "{config_path}/presets/ftrack/ftrack_config.json"
+        and returns data from this json
+    3.) else return None
+    '''
+    # config_path should be set from environments?
+    config_path = os.environ['PYPE_STUDIO_CONFIG']
+
+    preset_items = [config_path, 'presets']
+    preset_items.extend(args)
+    config_path = os.path.sep.join(preset_items)
+    if not os.path.isdir(config_path):
+        config_path += '.json'
+        if not os.path.exists(config_path):
+            log.error('Preset path was not found: "{}"'.format(config_path))
+            return None
+
+    return collect_json_path(config_path)
