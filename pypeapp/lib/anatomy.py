b8d4f8e6343d212651baee0a070c25914592ce84
Milan Kolar <milan@pype.club>
2019-03-21 10:11:56 +0100

    add simple mockup class

---
+++ pypeapp/lib/anatomy.py
@@ -0,0 +1,18 @@
+
+class Anatomy():
+
+    _discover():
+    '''
+    arg: 
+
+    find and load anatomy templates
+    '''
+
+    _solve_optional():
+
+
+    format():
+    '''
+
+    return solved, unsolved, partial
+    '''
