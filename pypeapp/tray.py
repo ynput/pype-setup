85b7d0e398c03f6986815bffec4bbc6979fc18da
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-22 19:06:07 +0100

    added working/wait widget to main_widget - not used yet (svg is used now, is ready to change to png and can be more modified)

--- pypeapp/tray.py
+++ pypeapp/tray.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 from pypeapp import style, Logger
-from Qt import QtCore, QtGui, QtWidgets
+from Qt import QtCore, QtGui, QtWidgets, QtSvg
 from pypeapp.lib.config import get_presets
 from pypeapp.resources import get_resource
 
@@ -320,24 +320,108 @@ class ServicesThread(QtCore.QThread):
 
 
 class TrayMainWindow(QtWidgets.QMainWindow):
+    """ TrayMainWindow is base of Pype application.
 
-    :param app: Qt app that hold all Qt widgets
+    Every widget should have set this window as parent because
+    QSystemTrayIcon widget is not allowed to be a parent of any widget.
+
+    :param app: Qt application manages application's control flow
     :type app: QtWidgets.QApplication
+
+    .. note::
+        *TrayMainWindow* has ability to show **working** widget.
+        Calling methods:
+            - ``show_working()``
+            - ``hide_working()``
+        TODO: Hide working widget if idle is too long
     """
     def __init__(self, app):
         super().__init__()
         self.app = app
 
+        self.set_working_widget()
 
         self.trayIcon = SystemTrayIcon(self)
         self.trayIcon.show()
 
+    def set_working_widget(self):
+        image_file = get_resource('working.svg')
+        img_pix = QtGui.QPixmap(image_file)
+        if image_file.endswith('.svg'):
+            widget = QtSvg.QSvgWidget(image_file)
+        else:
+            widget = QtWidgets.QLabel()
+            widget.setPixmap(img_pix)
+
+        # Set widget properties
+        widget.setGeometry(img_pix.rect())
+        widget.setMask(img_pix.mask())
+        widget.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint
+        )
+        widget.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+
+        self.center_widget(widget)
+        self._working_widget = widget
+        self.helper = DragAndDropHelper(self._working_widget)
+
+    def center_widget(self, widget):
+        frame_geo = widget.frameGeometry()
+        screen = self.app.desktop().cursor().pos()
+        center_point = self.app.desktop().screenGeometry(
+            self.app.desktop().screenNumber(screen)
+        ).center()
+        frame_geo.moveCenter(center_point)
+        widget.move(frame_geo.topLeft())
+
+    def show_working(self):
+        self._working_widget.show()
+
+    def hide_working(self):
+        self.center_widget(self._working_widget)
+        self._working_widget.hide()
+
+
+class DragAndDropHelper:
+    """ Helper adds to widget drag and drop ability
+
+    :param widget: Qt Widget where drag and drop ability will be added
+    """
+    def __init__(self, widget):
+        self.widget = widget
+        self.widget.mousePressEvent = self.mousePressEvent
+        self.widget.mouseMoveEvent = self.mouseMoveEvent
+        self.widget.mouseReleaseEvent = self.mouseReleaseEvent
+
+    def mousePressEvent(self, event):
+        self.__mousePressPos = None
+        self.__mouseMovePos = None
+        if event.button() == QtCore.Qt.LeftButton:
+            self.__mousePressPos = event.globalPos()
+            self.__mouseMovePos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            # adjust offset from clicked point to origin of widget
+            currPos = self.widget.mapToGlobal(
+                self.widget.pos()
+            )
+            globalPos = event.globalPos()
+            diff = globalPos - self.__mouseMovePos
+            newPos = self.widget.mapFromGlobal(currPos + diff)
+            self.widget.move(newPos)
+            self.__mouseMovePos = globalPos
+
+    def mouseReleaseEvent(self, event):
+        if self.__mousePressPos is not None:
+            moved = event.globalPos() - self.__mousePressPos
+            if moved.manhattanLength() > 3:
+                event.ignore()
+                return
+
 
 class Application(QtWidgets.QApplication):
-    """Main Qt application almost all pipeline widgets are running underneath.
-
-    .. note::
-        Contains *main_window* which should be used as parent for showing GUIs.
+    """Qt application manages application's control flow.
     """
     def __init__(self):
         super(Application, self).__init__(sys.argv)
