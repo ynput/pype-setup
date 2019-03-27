8c953cd5d29e66a269aa7c5c1821321bd48646c4
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-27 11:58:53 +0100

    modified anatomy test, still need more work

--- tests/test_anatomy.py
+++ tests/test_anatomy.py
@@ -3,7 +3,7 @@ from pypeapp import anatomy
 
 
 @pytest.fixture
-def valid_data(self, tmp_path):
+def valid_data(self):
     """ Provide valid sample of anatomy
 
         :param tmp_path: temporary path provided by pytest fixture
@@ -12,15 +12,21 @@ def valid_data(self, tmp_path):
         :rtype: string
     """
 
-    data = {"root": root,
-             "project": {"name": PROJECT,
-                         "code": project['data']['code']},
-             "silo": asset['silo'],
-             "asset": ASSET,
-             "family": instance.data['family'],
-             "subset": subset["name"],
-             "version": int(version["name"]),
-             "hierarchy": hierarchy}
+    data = {
+        'root': {
+            'work': 'ROOT_WORK'
+        },
+        'version': '1',
+        'hierarchy': 'HIER',
+        'ext': 'EXT',
+        'project': {
+            'code': 'PROJECT_CODE',
+            'name': 'PROJECT_NAME'
+        },
+        'asset': 'ASSET',
+        'task': 'TASK'
+    }
+
     # # json_path = tmpdir_factory.mktemp("deploy").join("valid-deploy.json")
     # json_path = tmp_path / "valid-deploy.json"
     # with open(json_path.as_posix(), "w") as write_json:
@@ -29,34 +35,51 @@ def valid_data(self, tmp_path):
 
 @pytest.fixture
 def valid_templates(self):
+    {
+        "resources": {
+            "footage": "{root[resources]}/{project[name]}/resources/footage",
+        }
 
-    resources:
-      footage: {root[resources]}/{project[name]}/resources/footage
-      reference: {root[resources]}/{project[name]}/resources/reference
+        "work": {
+            "file": "{project[code]}_{asset}_{task}_v{version:0>3}<_{comment}>.{ext}",
+            "path": "{root[publish]}/{project[name]}/{hierarchy}/{asset}/publish/{family}/{subset}/v{version:0>3}/{project[code]}_{asset}_{subset}_v{version:0>3}.{representation}"
+        }
 
-    work:
-      folder: {root[work]}/{project[name]}/{hierarchy}/{asset}/work/{task}
-      file: {project[code]}_{asset}_{task}_v{version:0>3}<_{comment}>.{ext}
-      path: {root[work]}/{project[name]}/{hierarchy}/{asset}/work/{task}/{asset}_{task}_v{version:0>3}<_{comment}>.{ext}
+        "avalon": {
+            "workfile": "{asset}_{task}_v{version:0>3}<_{comment}>",
+            "work": "{root}/{project[name]}/{hierarchy}/{asset}/work/{task}",
+            "publish": "{root}/{project[name]}/{hierarchy}/{asset}/publish/{family}/{subset}/v{version:0>3}/{project[code]}_{asset}_{subset}_v{version:0>3}.{representation}"
+        }
+    }
 
-    render:
-      path: {root[render]}/{project[name]}/{hierarchy}/{asset}/publish/render/{task}/{subset}/{version}/{project[code]}_{asset}_{task}_v{version:0>3}_{subset}<.{frame}>.{representation}
+@pytest.fixture
+def invalid_data(self):
+    """ Provide **invalid** sample of deployment.json
 
-    publish:
-      path: {root[publish]}/{project[name]}/{hierarchy}/{asset}/publish/{family}/{subset}/v{version:0>3}/{project[code]}_{asset}_{subset}_v{version:0>3}.{representation}
+        :param tmp_path: temporary path provided by pytest fixture
+        :type tmp_path: path
+        :returns: path to json file
+        :rtype: string
+    """
 
-    avalon:
-      workfile: {asset}_{task}_v{version:0>3}<_{comment}>
-      work: {root}/{project[name]}/{hierarchy}/{asset}/work/{task}
-      publish: {root}/{project[name]}/{hierarchy}/{asset}/publish/{family}/{subset}/v{version:0>3}/{project[code]}_{asset}_{subset}_v{version:0>3}.{representation}
-
+    data = {
+        'root': 'ROOT_WORK',
+        'version': '1',
+        'hierarchy': 'HIER',
+        'ext': 'EXT',
+        'project': {
+            'code': 'PROJECT_CODE',
+            'name': 'PROJECT_NAME'
+        },
+        'asset': {'name': 'ASSET'},
+        'task': 'TASK'
+    }
 
 def test_solve_optional(valid_templates, valid_data):
 
-    formatted_with_optional = formatting._solve_optional(valid_templates, valid_data)
-    formatted_no_optional = formatting._solve_optional(template, test_data_2)
-
-    valid_work_folder = "/prj/seq/sh010/car/work/animation"
+    anatomy = Anatomy()
+    formatted_with_optional = anatomy.(valid_templates, valid_data)
+    formatted_no_optional = anatomy.(template, test_data_2)
 
     assert formatted_with_optional == valid_work_folder
     assert formatted_no_optional == "iamrequiredKey"
