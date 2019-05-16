f3957e1edd5d9b20f31f5e066b32a919d0bc4379
Jakub Trllo <jakub.trllo@gmail.com>
2019-05-16 14:30:54 +0200

    ffmpeg init moved back to his folder

--- vendor/bin/__init__.py
+++ vendor/bin/__init__.py
@@ -3,32 +3,25 @@ import sys
 import importlib
 
 
-current_path = os.path.dirname(os.path.realpath(__file__))
-
-
-def install_ffmpeg():
-    '''
-    Adds ffmpeg into PATH so it's executable in terminal
-    '''
-    ffmpeg_path = os.path.join(current_path, 'ffmpeg_exec')
-    # MacOs - never tested
-    if sys.platform == 'darwin':
-        path_to_bin = os.path.join(ffmpeg_path, 'darwin')
-
-    # Windows
-    elif sys.platform == 'win32':
-        path_to_bin = os.path.join(ffmpeg_path, 'windows', 'bin')
-
-    # Linux
-    else:
-        path_to_bin = os.path.join(ffmpeg_path, 'linux')
-
-    if os.path.exists(path_to_bin):
-        os.environ['PATH'] += os.pathsep + path_to_bin
-
-
 def install():
-    install_ffmpeg()
+    '''
+    It's assumed that each bin app have __init__.py inside which sets
+    neccessary parts for usage. Only import of the package should set
+    it up.
+    '''
+    bin_path = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(bin_path)
+    with os.scandir(bin_path) as bin_folder:
+        for entry in bin_folder:
+            if not entry.is_dir():
+                continue
+            # os.path.basename had issues
+            name = os.path.split(entry.path)[-1]
+            try:
+                importlib.import_module(name)
+            except Exception:
+                print('Import of {} was not successful'.format(name))
+                pass
 
 
 install()
