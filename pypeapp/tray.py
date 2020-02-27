59192550219ffff59fefe3e28785fe61a5d5a749
iLLiCiTiT <jakub.trllo@gmail.com>
2020-02-27 12:11:45 +0100

    determine if in prod or dev by config.ini file

--- pypeapp/tray.py
+++ pypeapp/tray.py
@@ -5,6 +5,23 @@ from pypeapp import style, Logger
 from Qt import QtCore, QtGui, QtWidgets, QtSvg
 from pypeapp.lib.config import get_presets
 from pypeapp.resources import get_resource
+try:
+    import configparser
+except Exception:
+    import ConfigParser as configparser
+
+IS_DEV = False
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+config_file_path = os.path.join(cur_dir, "config.ini")
+if os.path.exists(config_file_path):
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    try:
+        value = config["DEFAULT"]["production"]
+        if value.lower() != "true":
+            IS_DEV = True
+    except KeyError:
+        pass
 
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
@@ -14,7 +31,12 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     :type parent: QtWidgets.QMainWindow
     """
     def __init__(self, parent):
-        self.icon = QtGui.QIcon(get_resource('icon.png'))
+        if IS_DEV:
+            icon_file_name = "icon_dev.png"
+        else:
+            icon_file_name = "icon.png"
+
+        self.icon = QtGui.QIcon(get_resource(icon_file_name))
 
         QtWidgets.QSystemTrayIcon.__init__(self, self.icon, parent)
 
@@ -482,7 +504,11 @@ class Application(QtWidgets.QApplication):
         self.splash.hide()
 
     def set_splash(self):
-        splash_pix = QtGui.QPixmap(get_resource('splash.png'))
+        if IS_DEV:
+            splash_file_name = "splash_dev.png"
+        else:
+            splash_file_name = "splash.png"
+        splash_pix = QtGui.QPixmap(get_resource(splash_file_name))
         self.splash = QtWidgets.QSplashScreen(splash_pix)
         self.splash.setMask(splash_pix.mask())
         self.splash.setEnabled(False)
