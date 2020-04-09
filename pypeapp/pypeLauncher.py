ced0e2129f816253ee31955933be58a6de8c12b8
iLLiCiTiT <jakub.trllo@gmail.com>
2020-04-09 18:26:47 +0200

    storage removed from pype setup

--- pypeapp/pypeLauncher.py
+++ pypeapp/pypeLauncher.py
@@ -306,7 +306,6 @@ class PypeLauncher(object):
         pass
 
     def _initialize(self):
-        from pypeapp.storage import Storage
         from pypeapp.deployment import Deployment
         from pypeapp.lib.Terminal import Terminal
 
@@ -323,7 +322,6 @@ class PypeLauncher(object):
                 config_path,
                 'environments'))
         self._add_modules()
-        Storage().update_environment()
         self._load_default_environments(tools=tools)
         self.print_info()
 
@@ -373,11 +371,6 @@ class PypeLauncher(object):
 
         self._initialize()
 
-        # find path from different platforms in environment and remap it to
-        # current platform paths. Only those paths specified in Storage
-        # will be remapped.
-        # os.environ.update(self.path_remapper())
-
         from pype import install, uninstall
         # Register target and host
         import pyblish.api
@@ -392,15 +385,7 @@ class PypeLauncher(object):
             t.echo("No publish paths specified")
             return False
 
-        # distinguish legacy remapping by "{root" in path
-        _paths = []
-        for path in paths:
-            if "{root" in path:
-                _paths.append(path)
-            else:
-                _paths.extend(self.path_remapper(path))
-
-        os.environ["PYPE_PUBLISH_DATA"] = os.pathsep.join(_paths)
+        os.environ["PYPE_PUBLISH_DATA"] = os.pathsep.join(paths)
 
         if gui:
             import pyblish_qml
@@ -544,114 +529,6 @@ class PypeLauncher(object):
         t.echo("*** For pype-setup: [ {} ]".format(build_dir_setup))
         t.echo("*** For pype: [ {} ]".format(build_dir_pype))
 
-    def path_remapper(self, data=None, source=None, to=None):
-        """
-        This will search existing environment for strings defined by variables
-        in storage setting for all platforms. If found, it will be replaced
-        with string from current platform.
-
-        For example, we have `PYPE_STORAGE_PATH` pointing on windows to
-        `V:\\Projects`. In environment setting, there is variable
-        `FOO="V:\\Projects\\Foo\\Bar\\baz"`. But we are on linux where
-        `PYPE_STORAGE_PATH` is defined as `/mnt/projects`. This method will
-        change `FOO` to point to `/mnt/projects/Foo/Bar/baz`.
-
-        This is useful on scenarios, where these variables are set on different
-        platform then one currently running.
-
-        Source data can be dictionary or list and it can contain other
-        dictionaries and lists. Paths will be replaced recursively.
-
-        :param data: Source data. By default it is `os.environ`
-        :type data: dict
-        :param source: string defining plaform from which we want to remap.
-                     If not set, then all platforms other then current one will
-                     be searched.
-        :type source: dict or list
-        :param to: string defining platform to which we want to remap. If not
-                   set, we will remap to current platform.
-        :returns: modified data
-        :rtype: dict or list
-        """
-        from pypeapp.storage import Storage
-
-        _platform_name = [
-            ("win32", "windows"),
-            ("linux", "linux"),
-            ("darwin", "darwin")
-        ]
-
-        _current_platform = [p[1] for p in _platform_name if p[0] == sys.platform][0]  # noqa: E501
-        if not data:
-            data = os.environ.copy()
-
-        if source:
-            from_paths_platform = [source]
-        else:
-            from_paths_platform = ['windows', 'linux', 'darwin']
-            from_paths_platform.remove(_current_platform)
-
-        if to:
-            to_paths_platform = to
-        else:
-            to_paths_platform = _current_platform  # noqa: E501
-
-        result_strings = Storage().get_storage_vars(platform=to_paths_platform)
-        if isinstance(data, dict):
-            remapped = {}
-        else:
-            remapped = []
-        done_keys = []
-        for p in from_paths_platform:
-            search_strings = Storage().get_storage_vars(platform=p)
-            for key in data:
-                if isinstance(data, dict):
-                    var = data[key]
-                else:
-                    var = key
-                # TODO: handle all cases. Normalized path, backslashes, ...
-                for skey, string in search_strings.items():
-                    # skip empty strings
-                    if not string:
-                        continue
-                    # work only on strings
-                    if not isinstance(var, str):
-                        # if another dict is found, recurse
-                        if isinstance(var, dict) or isinstance(var, list):
-                            # don't get into empty oness
-                            if var:
-                                revar = self.path_remapper(data=var,
-                                                           source=source,
-                                                           to=to)
-                                if isinstance(data, dict):
-                                    remapped[key] = revar
-                                else:
-                                    remapped.append(revar)
-                        else:
-                            if isinstance(data, dict):
-                                remapped[key] = var
-                            else:
-                                remapped.append(var)
-                        continue
-
-                    if string in var and key not in done_keys:
-                        out = var.replace(string, result_strings[skey])
-                        if to_paths_platform in ["win32", "windows"]:
-                            out = os.path.normpath(out)
-                        else:
-                            out = out.replace("\\", "/")
-                        if isinstance(data, dict):
-                            remapped[key] = out
-                        else:
-                            remapped.append(out)
-                        done_keys.append(key)
-                    elif key not in done_keys:
-                        if isinstance(data, dict):
-                            remapped[key] = var
-                        else:
-                            remapped.append(var)
-        return remapped
-
     def run_application(self, app, project, asset, task, tools, arguments):
         """
         Run application in project/asset/task context with default or
@@ -689,11 +566,6 @@ class PypeLauncher(object):
         import acre
         from avalon import lib
         from pype import lib as pypelib
-
-        # find path from different platforms in environment and remap it to
-        # current platform paths. Only those paths specified in Storage
-        # will be remapped.
-        # os.environ.update(self.path_remapper())
 
         abspath = lib.which_app(app)
         if abspath is None:
--- tests/test_pypeLauncher.py
+++ tests/test_pypeLauncher.py
@@ -1,7 +1,6 @@
 import os
 import pytest
 from pypeapp.pypeLauncher import PypeLauncher
-from .test_storage import TestStorage
 
 
 class TestPypeLauncher():
@@ -13,44 +12,3 @@ class TestPypeLauncher():
         PypeLauncher(['--traydebug'])
 
         assert os.environ.get('PYPE_DEBUG') == '3'
-
-    def test_path_remapping(self, tmp_path, monkeypatch):
-        test_env = {
-            "FOO": "orange:apple",
-            "BAR": "K:\\Windows\\Path\\Pear",
-            "BAZ": "/mnt/path/Pear",
-            "GOO": [
-                "/mnt/path/banana",
-                {"BOO": "/mnt/path/kiwi"}
-            ]
-        }
-
-        test_storage = """\
-{
-    "test": {
-        "dog": {
-            "path": {
-                "windows": "//store/mnt/path",
-                "linux": "/mnt/path",
-                "darwin": "/Volumes/path"
-            },
-            "mount": {
-                "windows": "K:\\\\Windows\\\\Path",
-                "linux": "/mnt/path",
-                "darwin": "/Volumes/path"
-            }
-        }
-    }
-}
-"""
-
-        TestStorage().setup_storage(test_storage, tmp_path, monkeypatch)
-        result = PypeLauncher().path_remapper(test_env, "windows", "linux")
-        assert result.get("BAR") == "/mnt/path/Pear"
-
-        result = PypeLauncher().path_remapper(test_env, "linux", "windows")
-        assert result.get("BAZ") == r"\\store\mnt\path\Pear"
-        assert result.get("BAR") == r"K:\Windows\Path\Pear"
-        assert result.get("FOO") == "orange:apple"
-        assert result.get("GOO")[0] == r"\\store\mnt\path\banana"
-        assert result.get("GOO")[1].get("BOO") == r"\\store\mnt\path\kiwi"
