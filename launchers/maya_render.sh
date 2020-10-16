7897187595e2449672949ba5925e825e4e6d392a
Ondrej Samohel <ondrej@samohel.cz>
2020-10-16 16:46:16 +0200

    handle space in Pype path in linux/osx

--- launchers/maya_render.sh
+++ launchers/maya_render.sh
@@ -4,5 +4,5 @@ echo ">>> running as:"
 who
 id -Gn
 echo ">>> launching mayabatch via pype with arguments $@"
-export PYPE_SETUP_PATH="$(cd $DIR/../ ; pwd)"
+export PYPE_SETUP_PATH=$(cd "$DIR"/../ ; pwd)
 source "$PYPE_SETUP_PATH/pype" launch --app mayabatch_$PYPE_MAYA_VERSION $@
