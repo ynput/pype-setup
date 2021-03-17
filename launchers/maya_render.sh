ad1119ec7c2b69f7d4fedfe909ec71ceda961fd5
Ondřej Samohel <33513211+antirotor@users.noreply.github.com>
2021-03-17 13:21:23 +0100

    find pype in maya farm launcher

--- launchers/maya_render.sh
+++ launchers/maya_render.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+realpath () {
+  echo $(cd $(dirname "$1") || return; pwd)/$(basename "$1")
+}
+
 export PYPE_LOG_NO_COLORS="1"
-echo ">>> running as:"
-who
-id -Gn
+echo ">>> running as: $(who)"
+
 echo ">>> launching mayabatch via pype with arguments $@"
-export PYPE_SETUP_PATH=$(cd "$DIR"/../ ; pwd)
+export PYPE_SETUP_PATH=$(dirname $(dirname "$(realpath ${BASH_SOURCE[0]})"))
 source "$PYPE_SETUP_PATH/pype" launch --app mayabatch_$PYPE_MAYA_VERSION $@
